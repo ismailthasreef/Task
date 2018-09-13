441b196004b0e5710b7e851d247a3e08aca138c7
amitechsolutionmlr <ciitech.edu@gmail.com>
2018-09-13 17:56:50 +0530

    some changes

---
+++ app/src/main/java/fyf/amitech/task/faculty.java
@@ -0,0 +1,32 @@
+package fyf.amitech.task;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class faculty extends Fragment {
+
+    private ListView listView;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        listView = (ListView) container.findViewById(R.id.facultylist);
+
+
+        return inflater.inflate(R.layout.faculty,container,false);
+
+
+    }
+}
